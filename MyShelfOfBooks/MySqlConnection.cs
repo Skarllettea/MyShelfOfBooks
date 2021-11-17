011108c65558065efac7b51996307cb85a91ba55
Skarllettea <dakotaroyz333@gmail.com>
2021-11-18 00:04:04 +0900

    Добавьте файлы проекта.

---
+++ MyShelfOfBooks/MySqlConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace MyShelfOfBooks
+{
+    class MySqlConnection
+    {
+        string strConnection = "server=localhost;user id=root;password=root;database=database_book";
+
+        public string CheckConnection()
+        {
+            string answer = string.Empty;
+            using (MySqlConnection connection = new MySqlConnection(strConnection))
+            {
+                connection.Open();
+                string SQL = "SELECT * FROM dbproject.people;";
+                MySqlCommand cmd = new MySqlCommand(SQL, connection);
+                answer = cmd.ExecuteScalar().ToString();
+            }
+
+            return answer;
+        }
+    }
+}
